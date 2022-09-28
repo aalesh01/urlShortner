37281cf3be0558584819b0efb0c59a79cc7c3a2f
aalesh01 <aalesh.patil@lendenclub.com>
2022-09-28 17:25:02 +0530

    done

--- server.js
+++ server.js
@@ -6,14 +6,25 @@ const shortUrl = require('./models/shortUrl');
 
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended: false}));
-app.get('/', (req, res) => {
-    res.render('index');
+app.get('/', async (req, res) => {
+    const allUrls = await shortUrl.find()
+    res.render('index',{shortUrls:allUrls});
 });
 
 app.post('/shortUrls', async (req, res) => {
        await shortUrl.create({ full: req.body.url });
        res.redirect('/');
     });
+
+app.get('/:shortUrls',async (req, res) =>{
+    const shortUrlF =  await shortUrl.findOne({ short: req.params.shortUrls });
+    if(shortUrlF===null) return res.status(404);
+    shortUrlF.clicks++;
+    shortUrlF.save()
+    res.redirect(shortUrlF.full);
+}
+    );
+
 
 app.listen(8080, () => {
     console.log('Server is running on port 8080');
